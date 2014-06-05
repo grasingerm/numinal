886c4af3231c3c80c7b0bc3bb67b7cdd6e6d9b77
Matthew Grasinger <grasingerm@gmail.com>
2014-06-04 23:59:29 -0400

    finished LU factorization problems

--- hw1/lu.cpp
+++ hw1/lu.cpp
@@ -110,6 +110,8 @@ vec lu_fb_substitution(mat &L, mat &U, vec &b)
 int main(int argc, char* argv[])
 {
     /* example 1, section 6.5, pg 391-392 */
+    cout << "Example 1, section 6.5, pg 391-392" << endl;
+    
     mat::fixed<4, 4> A;
     
     A << 1 << 1 << 0 << 3 << endr
@@ -167,8 +169,111 @@ int main(int argc, char* argv[])
             ASSERT_NEAR(U_actual(i,j), U(i,j), 1e-4);
         }
     }
-    /* finished tests */
     
+    /* 5d */
+    cout << "Problem 6.5.5.d, pg 396" << endl;
+    
+    mat::fixed<4,4> five_d;
+    five_d << 2.1756 << 4.0231 << -2.1732 << 5.1967 << endr
+           << -4.0231 << 6.0000 << 0. << 1.1973 << endr
+           << -1.000 << -5.2107 << 1.1111 << 0. << endr
+           << 6.0235 << 7.000 << 0. << -4.1561 << endr;
+           
+    cout << "matrix to factor: ... " << endl;
+    cout << five_d << endl;
+    
+    if (lu_mdoolittle(five_d, L, U) == FACTORIZATION_FAILURE)
+    {
+        cout << "ERROR: Factorization failed." << endl;
+        return 1;
+    }
+    
+    cout << "factorization..." << endl;
+    cout << "L = " << endl << L << endl;
+    cout << "U = " << endl << U << endl;
+    
+    /* end of 5d */
+    
+    /* 6d */
+    cout << "Problem 6.5.6.d, pg 397" << endl;
+    
+    mat::fixed<4,4> six_d;
+    six_d << 2.121 << -3.46 << 0. << 5.217 << endr
+           << 0. << 5.193 << -2.197 << 4.206 << endr
+           << 5.132 << 1.414 << 3.141 << 0. << endr
+           << -3.111 << -1.732 << 2.718 << 5.212 << endr;
+           
+    cout << "matrix to factor: ... " << endl;
+    cout << six_d << endl;
+    
+    if (lu_mdoolittle(six_d, L, U) == FACTORIZATION_FAILURE)
+    {
+        cout << "ERROR: Factorization failed." << endl;
+        return 1;
+    }
+    
+    cout << "factorization..." << endl;
+    cout << "L = " << endl << L << endl;
+    cout << "U = " << endl << U << endl;
+    
+    /* end of 6d */
+    
+    /* 7d */
+    cout << "Problem 6.5.7.d, pg 397" << endl;
+    
+    b(0) = 17.102;
+    b(1) = -6.1593;
+    b(2) = 3.0004;
+    b(3) = 0.;
+    
+    cout << "system of equations" << endl;
+    cout << five_d << endl << b << endl; // same matrix as in problem 5.d
+    
+    if (lu_mdoolittle(five_d, L, U) == FACTORIZATION_FAILURE)
+    {
+        cout << "ERROR: Factorization failed." << endl;
+        return 1;
+    }
+    
+    x = lu_fb_substitution(L, U, b);
+    cout << "solution..." << endl;
+    cout << "x = " << endl << x << endl;
+    
+    // test to see if factorization worked
+    mat b_test;
+    b_test = five_d * x;
+    for (int i = 0; i < 4; i++)
+        ASSERT_NEAR(b(i), b_test(i), 1e-4);
+    
+    /* end of 7d */
+    
+    /* 8d */
+    cout << "Problem 6.5.8.d, pg 397" << endl;
+    
+    b(0) = 1.909;
+    b(1) = 0.;
+    b(2) = -2.101;
+    b(3) = 6.824;
+    
+    cout << "system of equations" << endl;
+    cout << six_d << endl << b << endl; // same matrix as in problem 6.d
+    
+    if (lu_mdoolittle(six_d, L, U) == FACTORIZATION_FAILURE)
+    {
+        cout << "ERROR: Factorization failed." << endl;
+        return 1;
+    }
+    
+    x = lu_fb_substitution(L, U, b);
+    cout << "solution..." << endl;
+    cout << "x = " << endl << x << endl;
+    
+    // test to see if factorization worked
+    b_test = six_d * x;
+    for (int i = 0; i < 4; i++)
+        ASSERT_NEAR(b(i), b_test(i), 1e-4);
+    
+    /* end of 8d */
 
     return 0;
 }
