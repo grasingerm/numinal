e0a4da7af1eafc5679cd1afa999d5151b58b631e
Matthew Grasinger <grasingerm@gmail.com>
2014-06-10 13:22:01 -0400

    completed problem 4

--- hw1/ldl.cpp
+++ hw1/ldl.cpp
@@ -96,8 +96,68 @@ int main(int argc, char* argv[])
     for (unsigned int i = 0; i < A.n_rows; i++)
         for (unsigned int j = 0; j < A.n_cols; j++)
             ASSERT_NEAR(A(i,j), A_computed(i,j), 1e-5);
-    cout << " ... passed." << endl;
+    cout << " ... passed." << endl << endl;
     /* end of example 4 */
+    
+    
+    /**
+     * problem 4b, pg 410
+     */
+    mat::fixed<3,3> A_4b;
+    A_4b    <<  4   <<  2       <<  2       <<  endr
+            <<  2   <<  6       <<  2       <<  endr
+            <<  2   <<  2       <<  5       <<  endr;
+     
+    cout << "Problem 4b pg 410" << endl;
+    cout << "Matrix to be factorized, A = " << endl << A_4b << endl;
+    
+    assert( ldl_factorization(A_4b, L, D) == FACTORIZATION_SUCCESS );
+    
+    cout << "L = " << endl << L << endl;
+    cout << "D = " << endl << D << endl;
+    
+    /* compute A with L and D and check to make sure it equals the original */
+    A_computed = L * diagmat(D) * trans(L);
+    cout << "LDL' = " << endl << A_computed << endl;
+    
+    cout << "... testing A == LDL' ... ";
+    for (unsigned int i = 0; i < A_4b.n_rows; i++)
+        for (unsigned int j = 0; j < A_4b.n_cols; j++)
+            ASSERT_NEAR(A_4b(i,j), A_computed(i,j), 1e-5);
+    cout << " ... passed." << endl << endl;
+    /* end of problem 4b */
+    
+    /**
+     * problem 4d, pg 410
+     */
+    mat::fixed<4,4> A_4d;
+    A_4d    <<  4   <<  1       <<  1       <<  1   << endr
+            <<  1   <<  3       <<  0       <<  -1  << endr
+            <<  1   <<  0       <<  2       <<  1   << endr
+            <<  1   <<  -1      <<  1       <<  4   << endr;
+            
+    mat::fixed<4,4> L_4;
+    vec::fixed<4> D_4;
+    mat::fixed<4,4> A_computed_4;
+     
+    cout << "Problem 4d pg 410" << endl;
+    cout << "Matrix to be factorized, A = " << endl << A_4d << endl;
+    
+    assert( ldl_factorization(A_4d, L_4, D_4) == FACTORIZATION_SUCCESS );
+    
+    cout << "L = " << endl << L_4 << endl;
+    cout << "D = " << endl << D_4 << endl;
+    
+    /* compute A with L and D and check to make sure it equals the original */
+    A_computed_4 = L_4 * diagmat(D_4) * trans(L_4);
+    cout << "LDL' = " << endl << A_computed_4 << endl;
+    
+    cout << "... testing A == LDL' ... ";
+    for (unsigned int i = 0; i < A_4d.n_rows; i++)
+        for (unsigned int j = 0; j < A_4d.n_cols; j++)
+            ASSERT_NEAR(A_4d(i,j), A_computed_4(i,j), 1e-5);
+    cout << " ... passed." << endl << endl;
+    /* end of problem 4b */
 
     return 0;
 }
