5e5d06751ec7b3f0afecfdc5b4df64b665d8d621
Matthew Grasinger <grasingerm@gmail.com>
2014-11-12 17:25:29 -0500

    FINALLY fixed analytical solution to helmholtz equation

--- hw3/matlab/helmholtz.m
+++ hw3/matlab/helmholtz.m
@@ -2,21 +2,29 @@ function [A_n,u] = helmholtz (x_coords, y_coords, delta, n_terms)
     [garbage,n_x] = size(x_coords);
     [garbage,n_y] = size(y_coords);
     u = zeros(n_x,n_y);
-    A_n = zeros(n_terms, 1);
+    A_nm = zeros(n_terms, n_terms);
     
     for i=1:n_terms
-        n = 2*i-1;
-        A_n(i) = -(16.0 / (n^2 * pi^2 * (2 * n^2 * pi^2 + 1/delta) ));
+        for j=1:n_terms
+            n = 2*i-1;
+            m = 2*j-1;
+            A_nm(i,j) = (16.0 / 
+                ( (-n^2*pi^2 - m^2*pi^2 - 1/delta) * n*m*pi^2 )
+                );
+        end
     end
     
     for k=1:n_x
-        for j=1:n_y
+        for p=1:n_y
             x = x_coords(k);
-            y = y_coords(j);
+            y = y_coords(p);
         
             for i=1:n_terms
-                n = 2*i-1;
-                u(k,j) = u(k,j) + A_n(i) * sin(n*pi*x) * sin(n*pi*y);
+                for j=1:n_terms
+                    n = 2*i-1;
+                    m = 2*j-1;
+                    u(k,p) = u(k,p) + A_nm(i,j) * sin(n*pi*x) * sin(m*pi*y);
+                end
             end
         end
     end
